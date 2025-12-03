3789162729909bb2c8fac1c762ef9624a80dcc72
LambicJaune <thehopman@outlook.com>
2025-12-03 13:38:55 +0100

    Added authentification and protected views

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView   
 from .models import Recipe
+#to protect class-based view
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 def recipes_home(request):
    return render(request, 'recipes/recipes_home.html')
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipes/recipes_overview.html'
     context_object_name = 'recipes'
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipes/recipe_detail.html'
     context_object_name = 'recipe'
